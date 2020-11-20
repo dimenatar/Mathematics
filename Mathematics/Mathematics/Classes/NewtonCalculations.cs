df2cb36586b5ea60359c3c003a1c3a62efc20602
dimenatar <71691899+dimenatar@users.noreply.github.com>
2020-11-20 23:14:35 +0300

    Help me

--- Mathematics/Mathematics/Classes/NewtonCalculations.cs
+++ Mathematics/Mathematics/Classes/NewtonCalculations.cs
@@ -8,25 +8,13 @@ namespace Mathematics.Classes
 {
     static class NewtonCalculations
     {
-        public static double Cos()
+        public static double F(double x, double a)
         {
-            double Xn = 0.0, X = 0.0;
-            double f(double x)
-            {
-                return Math.Cos(2 / x) - 2 * Math.Sin(1 / x) + 1 / x;
-            }
-            double find(double a, double b, double e)
-            {
-                double E = 0.0;
-                Xn = b; //Здесь сами подумаете
-                do
-                {
-                    X = Xn - f(Xn) * e / (f(Xn + e) - f(Xn));
-                    E = Math.Abs(X - Xn);
-                    Xn = X;
-                } while (E > e);
-                return X;
-            }
+            return (Math.Cos(x-a));
         }
-    }
+        public static double dFdx(double x, double a)
+        {
+            return (Math.Sin(a - x));
+        }
+   }
 }
